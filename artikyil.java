7084cb2fe327040920a49506a6994630df1f60a1
necipfazilgocer <114177524+necipfazilgocer@users.noreply.github.com>
2022-09-25 16:22:05 +0300

    Add files via upload

---
+++ artikyil.java
@@ -0,0 +1,22 @@
+package artikyil;
+import java.util.Scanner;
+public class artikyil {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		int yil,kalan;
+		System.out.println("Bir yıl giriniz.");
+		yil=scanner.nextInt();
+		kalan=yil%4;
+		if(kalan==0)
+		{
+			System.out.println(yil +" Bir artık yıldır.");
+		}
+		
+		else 
+		{
+			System.out.println(yil+"Bir artık yıl değildir.");
+		}
+	}
+
+}
